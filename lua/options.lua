6236b7826aafcf94a8ed605415ffbfeb95287607
TeomanM <49800935+TeomanM@users.noreply.github.com>
2026-03-04 01:20:16 +0300

    fix(clipboard): Fix clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -48,8 +48,11 @@ g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
+
+-- set leader keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.g.clipboard = "wl-copy"
 
 o.laststatus = 3 -- global statusline
 o.showmode = false
