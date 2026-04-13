e4b8a59d362b31fa634f1a9e7c729abb6699b437
TeomanM <49800935+TeomanM@users.noreply.github.com>
2026-04-13 23:29:13 +0300

    refactor(autocmds): Remove LSP progress

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -35,22 +35,22 @@ vim.api.nvim_create_autocmd("DirChanged", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("LspProgress", {
-	callback = function(ev)
-		local value = ev.data.params.value or {}
-		local msg = value.message or "done"
-
-		-- rust analyszer in particular has really long LSP messages so truncate them
-		if #msg > 40 then
-			msg = msg:sub(1, 37) .. "..."
-		end
-		-- :h LspProgress
-		vim.api.nvim_echo({ { msg } }, false, {
-			id = "lsp",
-			kind = "progress",
-			title = value.title,
-			status = value.kind ~= "end" and "running" or "success",
-			percent = value.percentage,
-		})
-	end,
-})
+-- vim.api.nvim_create_autocmd("LspProgress", {
+-- 	callback = function(ev)
+-- 		local value = ev.data.params.value or {}
+-- 		local msg = value.message or "done"
+-- 
+-- 		-- rust analyszer in particular has really long LSP messages so truncate them
+-- 		if #msg > 40 then
+-- 			msg = msg:sub(1, 37) .. "..."
+-- 		end
+-- 		-- :h LspProgress
+-- 		vim.api.nvim_echo({ { msg } }, false, {
+-- 			id = "lsp",
+-- 			kind = "progress",
+-- 			title = value.title,
+-- 			status = value.kind ~= "end" and "running" or "success",
+-- 			percent = value.percentage,
+-- 		})
+-- 	end,
+-- })
